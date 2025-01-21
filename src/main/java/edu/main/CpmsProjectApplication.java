97f6956ec4a440e108f49a7948366860e8d2fb52
anugandhi22 <anuradhagandhi2001@gmail.com>
2025-01-21 20:29:32 +0530

    Category-Products

--- src/main/java/edu/main/CpmsProjectApplication.java
+++ src/main/java/edu/main/CpmsProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CpmsProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		System.out.println("Welcome....!!!");
 		System.out.println("Category & product mangement system");
 		SpringApplication.run(CpmsProjectApplication.class, args);
 	}
